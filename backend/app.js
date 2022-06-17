075d83f736eef89766d65b5f16ceac8400b93e77
dimitri-lev <dimitri.lev@protonmail.com>
2022-06-17 07:19:31 +0200

    update routes models

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://dim:TOd5WOMZESus8MmO@cluster0.73fvs.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ backend/controllers/sauce.js
@@ -0,0 +1,41 @@
+const Sauce = require("../models/Sauce");
+
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce)
+    console.log(req.body);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+        ...sauceObject,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [' '],
+        usersDisliked: [' '],
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    });
+    sauce.save()
+        .then(() => res.status(201).json({ message: "Sauce enregistrée" }))
+        .catch((error) => res.status(400).json({ error}));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => res.status(200).json(sauce))
+        .catch((error) => res.status(404).json({ error}));
+};
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then((sauces) => res.status(200).json(sauces))
+        .catch((error) => res.status(404).json({ error}));
+};
+
+exports.updateSauce = (req, res, next) => {
+    const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : { ...req.body }
+  
+    Sauce.updateOne({ _id : req.params.id}, {...sauceObject, _id: req.params.id})
+    .then(res.status(200).json({ message : "Sauce modifiée"}))
+    .catch(error => res.status(400).json({ error }))
+  }
