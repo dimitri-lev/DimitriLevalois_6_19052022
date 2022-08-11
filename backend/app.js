e231f730e8870b8d76e62659e8acd8b9fef92bbd
dimitri-lev <dimitri.lev@protonmail.com>
2022-08-11 10:38:13 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,6 @@ const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose
-  // .connect(process.env.SECRET_DB, {
   .connect(
     `mongodb+srv://${process.env.SECRET_DB_NAME}:${process.env.SECRET_DB_PASSWORD}@${process.env.SECRET_DB_HOST}/?retryWrites=true&w=majority`,
     {
