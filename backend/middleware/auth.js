a5cb9aa92bcf42cf7ad42cd9e3ba279682641989
dimitri-lev <dimitri.lev@protonmail.com>
2022-08-18 09:13:10 -0400

    ajout commentaire

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-    const userId = decodedToken.userId;
+    const token = req.headers.authorization.split(' ')[1]; // on récupère le token dans le header
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN); // on lui passe en argument le token récupéré et la clé secrète
+    const userId = decodedToken.userId; // on récupère notre userId puis on vérifier sa validité
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
